92dd5cf36a3fc071cb7061e5b0bba6a759ccf1d2
Gokhul26 <gokhul2005@gmail.com>
2024-09-22 18:23:37 +0530

    Rotated Binary Search Recursion Done

--- Recursion/returnAllIndex.java
+++ Recursion/returnAllIndex.java
@@ -6,8 +6,10 @@ public class returnAllIndex {
     public static void main(String[] args) {
         int[] arr = {1,2,2,3,4,5};
         int target = 2;
-        returnAllIndexValues(arr, target, 0);
-        System.out.println(list);
+        // returnAllIndexValues(arr, target, 0);
+        // System.out.println(list);
+        // System.out.println(returnAllIndexValuesArrayList(arr, target, 0,new ArrayList<>()));
+        System.out.println(returnAllIndexValuesArrayList2(arr, target, 0));
     }
     static ArrayList<Integer> list = new ArrayList<>();
     static void returnAllIndexValues(int[] arr,int target,int index) {
@@ -19,5 +21,29 @@ public class returnAllIndex {
         }
     
         returnAllIndexValues(arr, target, index+1); 
+    }
+
+    static ArrayList<Integer> returnAllIndexValuesArrayList(int[] arr,int target,int index,ArrayList<Integer> list) {
+        if(index == arr.length){
+            return list;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+    
+        return returnAllIndexValuesArrayList(arr, target, index+1,list); 
+    }
+    static ArrayList<Integer> returnAllIndexValuesArrayList2(int[] arr,int target,int index) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if(index == arr.length){
+            return list;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+    
+        ArrayList<Integer> ansfromBelowCalls = returnAllIndexValuesArrayList2(arr, target, index+1); 
+        list.addAll(ansfromBelowCalls);
+        return list;
     }
 }
