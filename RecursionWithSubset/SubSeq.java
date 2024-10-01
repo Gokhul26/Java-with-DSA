54c48f8bd5e4465c91bf141309080a2286aff94c
Gokhul26 <gokhul2005@gmail.com>
2024-10-01 09:02:14 +0530

    Iterative program for subset added

--- RecursionWithSubset/SubSeq.java
+++ RecursionWithSubset/SubSeq.java
@@ -1,9 +1,12 @@
 package RecursionWithSubset;
+import java.util.ArrayList;
 
 
 public class SubSeq {
     public static void main(String[] args) {
-        subseq("", "abc");
+        ArrayList<String> ans = subseqarrList("", "abc");
+        // subseqarrList("", "abc");
+        System.out.println(ans);
     }
 
     static void subseq(String proceesed, String unprocessed){
@@ -14,5 +17,22 @@ public class SubSeq {
         char ch = unprocessed.charAt(0);
         subseq(proceesed + ch , unprocessed.substring(1));
         subseq(proceesed, unprocessed.substring(1));
+    }
+    static ArrayList<String> subseqarrList(String processed,String unprocessed){
+        if(unprocessed.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        char ch = unprocessed.charAt(0);
+
+        ArrayList<String> left = subseqarrList(processed + ch, unprocessed.substring(1));
+        ArrayList<String> right = subseqarrList(processed , unprocessed.substring(1));
+        //ArrayList<String> ascii = subseqarrList(processed + (ch + 0), unprocessed.substring(1));
+
+        left.addAll(right);
+        // left.addAll(ascii);
+        return left;
     }
 }
