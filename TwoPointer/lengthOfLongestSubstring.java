e94e5a24690d31245c01d39cf878b177fdaa6bba
Gokhul26 <gokhul2005@gmail.com>
2024-10-11 10:56:01 +0530

    Lenght of the longest substring without duplicates added

---
+++ TwoPointer/lengthOfLongestSubstring.java
@@ -0,0 +1,27 @@
+package TwoPointer;
+import java.util.HashSet;
+import java.util.Set;
+
+public class lengthOfLongestSubstring {
+    public static void main(String[] args) {
+        String s = "asdfghjkl";
+        System.out.println(lenght(s));
+    }
+    static int lenght(String s){
+        int l = 0,r = 0,maxLength = 0;
+        Set<Character> set = new HashSet<>();
+        while( r < s.length()){
+            if(!set.contains(s.charAt(r))){
+                set.add(s.charAt(r));
+                r++;
+                maxLength = Math.max(maxLength, set.size());
+            }else{
+                set.remove(s.charAt(l));
+                l++;
+            }
+        }
+        return maxLength;
+        
+    }
+    
+}
